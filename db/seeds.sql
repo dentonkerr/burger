b8b597eceb133b7b8e4d5dcc882ead0f3dca9c8a
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2018-03-08 16:13:12 -0500

    adding progress, not working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ', false);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function() {
+    $(".change-devour").on("click", function(event) {
+        var id = $(this).data("id");
+        var newBurger = $(this).data("newburger");
+
+        var newBurgerState = {
+            devoured = newBurger
+        };
+
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: newBurgerState
+        }).then(
+            function() {
+                console.log("Change devoured to " + newBurger);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+        var newBurger = {
+            burger_name: $("#burg").val().trim(),
+            devoured: false
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Added New Burger");
+                location.reload();
+            }
+        );
+    });
+
+});
